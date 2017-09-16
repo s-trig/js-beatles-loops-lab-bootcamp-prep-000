3c1b14d94e6cb6755513c5db077a639a831dc3da
David Reep <davereep@gmail.com>
2017-09-16 22:28:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,8 @@ function johnLennonFacts(facts) {
 function iLoveTheBeatles(number) {
   var array = []
   do {
-    array.push("I love the Beatles!")
+    array.push("I love the Beatles!");
+    number++
   } while {
     number > 15
   }
