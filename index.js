7b34b3b55b2e5be171b2588bd9f54ee0bb2c7eab
David Reep <davereep@gmail.com>
2017-09-16 22:30:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,6 @@ function iLoveTheBeatles(number) {
   do {
     array.push("I love the Beatles!");
     number++
-  } while {
-    number < 15
-  }
-  return array
+  } while (number < 15)
+    return array
 }
